3baecbdc4bb359fa6f03195bd3489877fd6b1f9d
Caperro20 <Caperro@6qy01j.onmicrosoft.com>
2025-03-04 17:40:24 -0600

    Hice mi presentacion

---
+++ src/Presentacion.java
@@ -0,0 +1,8 @@
+public class Presentacion {
+    public static void main(String[] args) {
+        System.out.println("Hola Me llamo ");
+        System.out.println("Juan Manuel López de la Cruz ");
+        System.out.println("Vivo en Mexico ");
+        System.out.println("Soy Progrmador");
+    }
+}
