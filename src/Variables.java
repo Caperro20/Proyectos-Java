01441aa230d970c61802480574209b28d52657b8
Caperro20 <Caperro@6qy01j.onmicrosoft.com>
2025-03-04 18:16:04 -0600

    use variables para hacer una presentación

---
+++ src/Variables.java
@@ -0,0 +1,8 @@
+public class Variables {
+    public static void main(String[] args) {
+        int edad = 30;
+        double sueldo = 15020.50;
+        String nombre = "Juan Manuel";
+        System.out.println("El es " +  nombre + " el tiene una edad de " + edad + " años" + " y gana un sueldo de " + sueldo + " al mes");
+    }
+}
